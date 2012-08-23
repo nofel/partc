fba4cf686850595b03d17bedfc5a4ce73d685188
Nofel <s3254869@student.rmit.edu.au>
2012-08-24 00:53:22 +1000

    mentain the error if the connection can't be established

---
+++ executequery.php
@@ -0,0 +1,40 @@
+<?php
+error_reporting(E_ALL);
+
+// include the database configuration
+require_once('dbconfig.php');
+
+define("USER_HOME_DIR", "/home/stud/s3254869");
+require(USER_HOME_DIR . "/php/Smarty-2.6.26/Smarty.class.php");
+$smarty = new Smarty();
+$smarty->template_dir = USER_HOME_DIR . "/php/Smarty-Work-Dir/templates";
+$smarty->compile_dir = USER_HOME_DIR . "/php/Smarty-Work-Dir/templates_c";
+$smarty->cache_dir = USER_HOME_DIR . "/php/Smarty-Work-Dir/cache";
+$smarty->config_dir = USER_HOME_DIR . "/php/Smarty-Work-Dir/configs";
+
+if (!($connection = @ mysql_connect(DB_HOST . ":" . DB_PORT, DB_USER, DB_PW)))
+{
+	display_error();
+}
+
+// if database can not be selected, then show error
+if (!mysql_select_db('winestore', $connection))
+{
+	display_error();
+}
+
+
+$region = clean_sql($_GET, "region", 4, $connection);
+$startyear = clean_sql($_GET, "startyear", 4, $connection);
+$mincost = clean_sql($_GET, "mincost", 50, $connection);
+$maxcost = clean_sql($_GET, "maxcost", 50, $connection);
+$wine = clean_sql($_GET, "wine", 50, $connection);
+$winery = clean_sql($_GET, "winery", 100, $connection);
+$endyear = clean_sql($_GET, "endyear", 4, $connection);
+$stocknum = clean_sql($_GET, "stocknum", 5, $connection);
+
+$errordisplay ='';
+
+
+
+
