089da90fb3adf5e06e3ae611c67bf10b7554810c
Nofel <s3254869@student.rmit.edu.au>
2012-08-23 23:33:27 +1000

    i have used the configuration for Part-B

--- dbconfig.php
+++ dbconfig.php
@@ -15,3 +15,22 @@ define('DB_PW', '4getmenot');
 
 $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME;
 
+// Function to show an error to the user
+function display_error()
+{
+  die("An error has occurred" . mysql_errno() . " : " . mysql_error());
+}
+
+// Function to clean an array
+// This is the security feature - to get rid of all special character 
+(convert to escape sequence)
+function clean_sql($array, $index, $maxlength, $connection)
+{
+  if (isset($array["{$index}"]))
+  {
+     $input = substr($array["{$index}"], 0, $maxlength);
+     $input = mysql_real_escape_string($input, $connection);
+     return ($input);
+  }
+  return NULL;
+}
