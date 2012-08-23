d2d92b72c49614a196ac7be52c26195a62eee817
Nofel <s3254869@student.rmit.edu.au>
2012-08-23 23:30:11 +1000

    I have created the database configuration

---
+++ dbconfig.php
@@ -0,0 +1,17 @@
+</php
+/*
+  Nofel Shanta
+  Student ID: s3254869
+  Web Database Applicaton - Assignment1
+  Part B - Develop a two component query module.
+  Date 23/08/2012
+*/
+
+define('DB_HOST', 'yallara.cs.rmit.edu.au');
+define('DB_PORT', '54335');
+define('DB_NAME', 'winestore');
+define('DB_USER', 'root');
+define('DB_PW', '4getmenot');
+
+$dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME;
+
