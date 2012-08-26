d6bcadbd9e4a96a95d13ab2a502f0563d30e6a09
Nofel <s3254869@student.rmit.edu.au>
2012-08-26 22:58:22 +1000

    I have and changed some lines

--- queryform.php
+++ queryform.php
@@ -1,16 +1,16 @@
-/*
-	Nofel Shanta
-	Student ID: s3254869
-	Web Database Application - Assignment 1
-	Part C - Create Part B using Smarty Template
-	Date 24/08/2012
-*/
 <?php
+/*
+     Nofel Shanta
+     Student ID: s3254869
+     Web Database Application - Assignment 1
+     Part C - Create Part B using Smarty Template
+*/
+
 require_once('dbconfig.php');
 ?>
 
 <?php
-error_reporting(E_ALL);
+          error_reporting(E_ALL);
 	  define("USER_HOME_DIR", "/home/stud/s3254869");
 	  require(USER_HOME_DIR . "/php/Smarty-2.6.26/Smarty.class.php");
 	  $smarty = new Smarty();
@@ -25,6 +25,7 @@ error_reporting(E_ALL);
           /* Populate the region dropdown */
 	  $region = array();
 	  $default = '';
+
           /* populate the list of regions from the database */
 	  $pdoobject = new PDO($dsn, DB_USER, DB_PW);
 	  $pdoobject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -46,7 +47,8 @@ error_reporting(E_ALL);
 	  /* Populate the start year dropdown */
 	  $yeararr = array();
 	  $default = '';
-	  // populate the list of regions from the database
+
+	  /* populate the list of regions from the database */
 	  $pdoobject = new PDO($dsn, DB_USER, DB_PW);
 	  $pdoobject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	  $query = 'select distinct year from wine order by year asc';
@@ -62,11 +64,13 @@ error_reporting(E_ALL);
 	  $smarty->assign('startyearOptions', $yeararr);
 	  $smarty->assign('startyearSelect', $default);
 ?>
+
 <?php
 	  /* Populate the end year dropdown */
 	  $yeararr = array();
 	  $default = '';
-	  // populate the list of regions from the database
+
+	  /* populate the list of regions from the database */
 	  $pdoobject = new PDO($dsn, DB_USER, DB_PW);
 	  $pdoobject->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	  $query = 'select distinct year from wine order by year asc';
